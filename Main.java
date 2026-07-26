7a263b7df6bfee627aa1f0051769c6bcd99bba96
jananisusila <jananishanmugam2007@gmail.com>
2026-07-26 18:34:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+    public class Main {
+    public static void main (String [] args){
+    int arr[]= new int[5];
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    arr[3]=40;
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+    System.out.println(arr[3]);
+   
+    }
+    
+}
+
